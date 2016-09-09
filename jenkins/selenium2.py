d22f2e2790ce3da80dd87993a8560945cca2488d
wing-blazemeter <wing@blazemeter.com>
2016-09-09 11:24:46 -0700

    Add files via upload

---
+++ jenkins/selenium2.py
@@ -0,0 +1,30 @@
+# ------------------------ Don't modify ---------------------------
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+class PythonOrgSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.google.com")
+        assert "Google" in driver.title
+        elem = driver.find_element_by_name('q')
+        elem.clear()
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+        unittest.main()
+
+# ------------------------ Don't modify ---------------------------
+
